7895a55d1cc8ea7572aba95e25d10c4d6aaf807c
paulliu87 <paul.pliu87@gmail.com>
2017-07-19 16:52:39 -0700

    create a simple app that catch the qr code and change virtual box's color

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+// var sceneEl = document.querySelector('a-scene');
+$(document).ready(function() {
+	var box = $('a-box');
+	var button = $("#button");
+
+	// Add event listener with `addEventListener`.
+	console.log("hello");
+	$('#button').on('click', function (e) {
+	  // Modify entity with `setAttribute`.
+	  console.log("clicked!");
+	  box.attr('color','blue');
+	});
+
+
+	// Get data about entity with `getAttribute`.
+	console.log("hellp");
+	// >> {x: -1, y: 0.5, z: -3}
+
+	// Add event listener with `addEventListener`.
+	box.on('click', function () {
+	  // Modify entity with `setAttribute`.
+	  	box.setAttribute('color', 'green');
+	});
+})
